1943206b2ad0bdfbfeda8be992373c7b1e51b631
Andres Estrada <aestrad494@gmail.com>
2019-08-19 20:25:26 -0500

    converting to Object Oriented

--- Backtesting_Class.py
+++ Backtesting_Class.py
@@ -4,10 +4,26 @@ import numpy as np
 class Backtesting_Strategy():
     def __init__(self,df):
         self.df = df
-        
-    def entry_days(self):
-        entry_days = ((self.df['final profit buy'] + self.df['final profit sell']) != 0).sum()
-        return(entry_days)
-        
-        
-        
+    
+    def calc_commission(self, shares):
+        commission = shares * 0.005
+        if (commission < 1):
+            commission = 1
+        return(commission)
+    
+    def final_profit_usd(self):
+        total_profit_usd = round((self.df['profit usd']).sum(),2)
+        return(total_profit_usd)
+    
+    def total_commissions(self):
+        commissions_per_trade = self.df['lots'].apply(self.calc_commission)
+        trades_per_day = (self.df['final profit buy'] != 0)*2 + (self.df['final profit sell'] != 0)*2
+        total_commissions = (commissions_per_trade * trades_per_day).sum()
+        return(round(total_commissions,2))
+    
+    def gross_profit_and_loss(self):
+        results_long = self.df['final profit buy'] * self.df['lots']
+        results_short = self.df['final profit sell'] * self.df['lots']
+        total_profits = round(results_long[results_long > 0].sum() + results_short[results_short > 0].sum(),2)
+        total_losses = round(results_long[results_long < 0].sum() + results_short[results_short < 0].sum(),2)
+        return([total_profits,total_losses])
